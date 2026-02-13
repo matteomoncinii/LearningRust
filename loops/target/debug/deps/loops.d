7bcdb30952eb1b0fca3e117ac89d2cb41f899164
m.moncini <m.moncini@frigel.com>
2026-02-13 13:02:27 +0100

    loops

---
+++ loops/target/debug/deps/loops.d
@@ -0,0 +1,5 @@
+C:\Users\m.moncini\Repositories\LearningRust\LearningRust\loops\target\debug\deps\loops.d: src\main.rs
+
+C:\Users\m.moncini\Repositories\LearningRust\LearningRust\loops\target\debug\deps\loops.exe: src\main.rs
+
+src\main.rs:
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+C:\Users\m.moncini\Repositories\LearningRust\LearningRust\loops\target\debug\loops.exe: C:\Users\m.moncini\Repositories\LearningRust\LearningRust\loops\src\main.rs
