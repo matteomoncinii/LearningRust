c2d7a8b394f548f0056ce32f12e93bbab4ec7471
m.moncini <m.moncini@frigel.com>
2026-03-13 13:01:51 +0100

    ownership - part 3

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -53,6 +53,26 @@ fn main() {
     // Variables r1 and r2 will not be used after this point.
     let ref3 = &mut s5; // no problem
     println!("Reference 3: {ref3}");
+
+    // THE SLICE TYPE
+    println!("\n--- The Slice Type");
+    let my_string = String::from("hello world");
+    // `first_word` works on slices of `String`s, whether partial or whole.
+    let word = first_word(&my_string[0..5]);
+    println!("Input as slice of the string: {word}");
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s.
+    let word = first_word(&my_string);
+    println!("Input as a reference to the string: {word}");
+    let my_string_literal = "hello world";
+    // `first_word` works on slices of string literals, whether partial or
+    // whole.
+    let word = first_word(&my_string_literal[0..6]);
+    println!("Input as a slice of the string literal: {word}");
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+    println!("Input as a string literal (which is a slice itself){word}");
 }
 
 fn takes_ownership(some_string: String) {
@@ -88,4 +108,14 @@ fn takes_reference(ref_string: &String) -> usize {
 
 fn takes_ref_and_change(some_string: &mut String) {
     some_string.push_str(", world");
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
 }
